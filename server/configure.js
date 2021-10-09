646bfadedf427d0ea8bb60f5e10b51e2545f8b2e
gosteve <nomuojamaro@gmail.com>
2021-10-10 00:29:37 +0100

    contollers and server config added

--- server/configure.js
+++ server/configure.js
@@ -1,5 +1,5 @@
 var path = require('path'),
- //routes = require('./routes'),
+ routes = require('./routes'),
  exphbs = require('express-handlebars'),
  express = require('express'),
  bodyParser = require('body-parser'),
@@ -7,19 +7,21 @@ var path = require('path'),
  morgan = require('morgan'),
  methodOverride = require('method-override'),
  errorHandler = require('errorhandler');
+module.exports = function(app) {
+app.use(morgan('dev'));
 
- module.exports = function(app) {
-    app.use(morgan('dev'));
-    app.use(bodyParser.urlencoded({ extended: true }));
-     app.use(bodyParser.json());
-     app.use(methodOverride());
-     app.use(cookieParser('some-secret-value-here'));
-     //routes(app);
-     //moving the routes to routes folder.
-     app.use('/public/', express.static(path.join(__dirname,
-        '../public')));
-       if ('development' === app.get('env')) {
-        app.use(errorHandler());
-       }
-        return app;
-       };
+ app.use(bodyParser({
+ uploadDir:path.join(__dirname, 'public/upload/temp')
+}));
+
+ app.use(methodOverride());
+ app.use(cookieParser('some-secret-value-here'));
+ routes(app);
+ 
+ app.use('/public/', express.static(path.join(__dirname,
+ '../public')));
+ if ('development' === app.get('env')) {
+   app.use(errorHandler());
+  }
+   return app;
+  };
