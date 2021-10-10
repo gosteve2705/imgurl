1858f74cee827f6309ded4782bb4603da556b89d
gosteve <nomuojamaro@gmail.com>
2021-10-10 23:32:19 +0100

    image and index view created

--- server/configure.js
+++ server/configure.js
@@ -22,6 +22,12 @@ app.use(morgan('dev'));
  if ('development' === app.get('env')) {
    app.use(errorHandler());
   }
+  app.engine('handlebars', exphbs.create({
+    defaultLayout: 'main',
+    layoutsDir: app.get('views') + '/layouts',
+    partialsDir: [app.get('views') + '/partials']
+   }).engine);
+   app.set('view engine', 'handlebars');
    return app;
   };
   
