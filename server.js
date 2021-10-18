e05254a5ffd24ab3d25ff6658f02660f464c4924
gosteve <nomuojamaro@gmail.com>
2021-10-18 06:56:00 +0100

    adding mongoose ability

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express'),
  config = require('./server/configure'),
+ mongoose = require('mongoose'),
 app = express();
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
  app = config(app);
+ mongoose.connect('mongodb://localhost/imgPloadr');
+mongoose.connection.on('open', function() {
+console.log('Mongoose connected.');
+});
 //app.get('/', function(req, res){
  //   res.send('Hello World');
  //  });
