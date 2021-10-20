30085e21c4d0a3482451f66a3a8ee2f7acf41928
gosteve <nomuojamaro@gmail.com>
2021-10-20 06:32:51 +0100

    refactored models into the project root

--- controllers/home.js
+++ controllers/home.js
@@ -1,17 +1,19 @@
-var sidebar = require('../helpers/sidebar'),
-ImageModel = require('../models').Image;
+var sidebar = require("../helpers/sidebar"),
+  ImageModel = require("../models").Image;
 module.exports = {
   index: function (req, res) {
-    var viewModel = {
-      images: [],
-    };
-    ImageModel.find({}, {}, { sort: { timestamp: -1 }},
-      function(err, images) {
-      if (err) { throw err; }
+    var viewModel = { images: [] };
+    ImageModel.find({}, {}, { sort: { timestamp: -1 } }, function (
+      err,
+      images
+    ) {
+      if (err) {
+        throw err;
+      }
       viewModel.images = images;
-      sidebar(viewModel, function(viewModel) {
-      res.render('index', viewModel);
-       });
-       });
+      sidebar(viewModel, function (viewModel) {
+        res.render("index", viewModel);
+      });
+    });
   },
 };
